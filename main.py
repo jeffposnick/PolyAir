719a98f91b732a17aaf123441f4d438433f300a6
Jeff Posnick <jeffy@google.com>
2015-01-30 17:09:19 -0500

    Some basic changes.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2015 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,38 +14,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
-import urllib
-import logging
-import datetime
+
 import json
+import logging
+import urllib
+import webapp2
 from google.appengine.api import urlfetch
 from google.appengine.ext import db
+
 
 class PushRegistration(db.Model):
   registration = db.StringProperty(required=True)
   endpoint = db.StringProperty(required=True)
 
+
 def clearRegistrations():
   for r in db.GqlQuery("SELECT * FROM PushRegistration"):
     r.delete()
 
+def read_auth_key():
+  import os.path
+  folder = os.path.dirname(os.path.realpath(__file__))
+  file_path = os.path.join(folder, 'auth_key')
+  with open(file_path) as auth_key_file:
+    return auth_key_file.readline().rstrip()
+
+auth_key = read_auth_key()
+logging.info('auth key is ' + auth_key)
+
 class ClearRegistrationsHandler(webapp2.RequestHandler):
   def post(self):
-    clearRegistrations();
-    self.response.write('{ "success": true }');
+    clearRegistrations()
+    self.response.write('{ "success": true }')
+
 
 class PushHandler(webapp2.RequestHandler):
   def post(self):
     logging.info('PushHandler')
 
-    registrations = db.GqlQuery("SELECT * FROM PushRegistration");
-    if registrations.count() == 0 :
+    registrations = db.GqlQuery("SELECT * FROM PushRegistration")
+    if registrations.count() == 0:
       self.response.write('{ "success": false, "message": "No registration available to use" }')
-      return;
+      return
 
-    registrationId = registrations[0].registration;
-    endpoint = db.GqlQuery("SELECT * FROM PushRegistration")[0].endpoint;
+    registrationId = registrations[0].registration
+    endpoint = db.GqlQuery("SELECT * FROM PushRegistration")[0].endpoint
 
     logging.info(registrationId)
     logging.info(endpoint)
@@ -64,41 +77,44 @@ class PushHandler(webapp2.RequestHandler):
                             payload=form_data,
                             method=urlfetch.POST,
                             headers={
-              'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-              'Authorization': 'key=AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ'
-            })
-    
-    if result.status_code == 200 and not result.content.startswith("Error") :
+                              'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                              'Authorization': auth_key
+                            })
+
+    if result.status_code == 200 and not result.content.startswith("Error"):
       self.response.write('{ "success": true, ' +
                           '"registration": "' + registrationId + '" }')
     else:
       logging.info(result.content)
       self.response.write('{ "success": false }')
 
+
 class PushRegistrationHandler(webapp2.RequestHandler):
   def post(self):
-    clearRegistrations();
-    
+    clearRegistrations()
+
     logging.warning(self.request.get("registration"))
     logging.warning(self.request.get("endpoint"))
 
     p = PushRegistration(
-      registration = self.request.get("registration"),
-      endpoint = self.request.get("endpoint"));
+      registration=self.request.get("registration"),
+      endpoint=self.request.get("endpoint"))
     p.put()
 
     self.response.write('{ "success": true }')
+
 
 class GetRegistrationsHandler(webapp2.RequestHandler):
   def post(self):
     response = ""
     for r in db.GqlQuery("SELECT * FROM PushRegistration"):
       response += r.registration + ", "
-    self.response.write('{ "response": "' + response + '" }');
+    self.response.write('{ "response": "' + response + '" }')
+
 
 app = webapp2.WSGIApplication([
-  ('/push', PushHandler),
-  ('/register_track', PushRegistrationHandler),
-  ('/clear_registrations', ClearRegistrationsHandler),
-  ('/get_registrations', GetRegistrationsHandler)
-], debug=True)
+                                ('/push', PushHandler),
+                                ('/register_track', PushRegistrationHandler),
+                                ('/clear_registrations', ClearRegistrationsHandler),
+                                ('/get_registrations', GetRegistrationsHandler)
+                              ], debug=True)
